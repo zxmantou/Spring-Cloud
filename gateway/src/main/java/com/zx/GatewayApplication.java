8e9928574caddd494641e8d9f2c93c06f985e48c
zhangx-ae <zhangx-ae@glodon.com>
2017-03-24 17:29:30 +0800

    gateway

---
+++ gateway/src/main/java/com/zx/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.zx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
+
+	}
+}
