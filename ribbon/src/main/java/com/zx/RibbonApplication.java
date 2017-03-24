da80fa6ca213db11856144978a9973f4d8f28347
zhangx-ae <zhangx-ae@glodon.com>
2017-03-24 15:56:18 +0800

    config server

--- ribbon/src/main/java/com/zx/RibbonApplication.java
+++ ribbon/src/main/java/com/zx/RibbonApplication.java
@@ -2,6 +2,7 @@ package com.zx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -14,8 +15,9 @@ import org.springframework.web.client.RestTemplate;
 public class RibbonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RibbonApplication.class, args);
-	}
+        new SpringApplicationBuilder(RibbonApplication.class).web(true).run(args);
+    }
+
 
 	@Bean
 	@LoadBalanced
