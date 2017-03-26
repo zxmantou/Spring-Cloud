3443ac99ab562f182dcfdb5eea5655cf6458bbf6
Zhangx-ae <zhangx-ae@glodon.com>
2017-03-27 07:33:31 +0800

    gateway filter

--- gateway/src/main/java/com/zx/GatewayApplication.java
+++ gateway/src/main/java/com/zx/GatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -15,5 +16,10 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(GatewayApplication.class).web(true).run(args);
 
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
